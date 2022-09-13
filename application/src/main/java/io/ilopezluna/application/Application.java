1ad21d2a39bc68b2b404907a1535a196bd633832
ignasi <ignasi.lopez.luna@gmail.com>
2022-09-13 22:21:40 +0200

    Print args + env values on startup

--- application/src/main/java/io/ilopezluna/application/Application.java
+++ application/src/main/java/io/ilopezluna/application/Application.java
@@ -2,15 +2,24 @@ package io.ilopezluna.application;
 
 import io.ilopezluna.application.entities.Note;
 import io.ilopezluna.application.repositories.NoteRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
+
 @SpringBootApplication
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
+        log.info("Args:");
+        log.info(Arrays.toString(args));
+
+        log.info("Env:");
+        log.info(System.getenv().toString());
         SpringApplication.run(Application.class, args);
     }
 
